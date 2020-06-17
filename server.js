2fc1400ff7dd474c8dfec99a37143955ae4ffb2e
Akashdeep <adeep8961@gmail.com>
2020-06-17 15:55:07 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express')
+
+const app = express()
+const PORT = process.env.PORT || 5000
+app.get('/', (req, res) => res.send('App running'))
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
